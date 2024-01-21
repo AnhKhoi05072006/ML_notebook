a08f3f6d923e1182f06b3c53c295cc89d69eee49
s1ckduck <61281333+AnhKhoi05072006@users.noreply.github.com>
2024-01-21 21:19:43 +0700

    derivative-implt.py

---
+++ linear regression/derivative-implt.py
@@ -0,0 +1,10 @@
+def linear_regression(x,y):
+    one = np.ones((x.shape[0],1))
+    x = np.concatenate((x,one), axis = 1)
+    ANS = np.dot(np.linalg.pinv(x), y)
+    return ANS
+
+def predict(x,w):
+    one = np.ones((x.shape[0],1))
+    x = np.concatenate((x,one), axis = 1)
+    return np.dot(x,w)
